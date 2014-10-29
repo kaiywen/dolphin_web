33d1d41a38d8dc1c16bb5f31fe17b98e129e7fdf
yuanbaohundan <wenkaiyuan@dislab.nju.edu.cn>
2014-10-30 01:02:47 +0800

    Version 1.4

--- mysite/media/js/my_index.js
+++ mysite/media/js/my_index.js
@@ -1,7 +1,7 @@
 $("#s_exec_ipmi").click(function() {
-    $.blockUI({ //当点击事件发生时调用弹出层
-        message: $('#box'), //要弹出的元素box
-        css: { //弹出元素的CSS属性
+    $.blockUI({
+        message: $('#box'), 
+        css: {
             top: '23%',
             left: '40%',
             textAlign: 'center',
@@ -11,6 +11,6 @@ $("#s_exec_ipmi").click(function() {
             background: 'none'
         }
     });
-    $('.blockOverlay').attr('title', '单击关闭').click($.unblockUI);
+    $('.blockOverlay').attr('title', 'shutdown').click($.unblockUI);
 });
 
